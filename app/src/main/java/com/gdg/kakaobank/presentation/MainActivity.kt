c8a67e10c05182196ef114905de713eb6386ac73
nhyeonii <nhyeonii@naver.com>
2024-11-20 19:00:40 +0900

    [Refactor] 네비게이션 바 클릭시 생성되는 Ripple 제거

--- app/src/main/java/com/gdg/kakaobank/presentation/MainActivity.kt
+++ app/src/main/java/com/gdg/kakaobank/presentation/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,6 +23,7 @@ import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.*
+import androidx.navigation.compose.rememberNavController
 import com.gdg.kakaobank.ui.theme.KakaoBankTheme
 import com.gdg.kakaobank.presentation.navigator.KakaoNav
 import com.gdg.kakaobank.ui.theme.B4_R
@@ -63,47 +65,50 @@ fun MainScreen() {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         bottomBar = {
-            BottomNavigation(
-                backgroundColor = White,
-            ) {
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentRoute = navBackStackEntry?.destination?.route
+                BottomNavigation(
+                    backgroundColor = White,
+                ) {
+                    val navBackStackEntry by navController.currentBackStackEntryAsState()
+                    val currentRoute = navBackStackEntry?.destination?.route
 
-                items.forEach { screen ->
-                    NavigationBarItem(
-                        label = {
-                            Text(text = stringResource(id = screen.resourceId),
-                            style = B4_R
-                            )
-                        },
-                        selected = currentRoute == screen.route,
-                        colors = NavigationBarItemDefaults.colors(
-                            selectedTextColor = Dark_Gray,
-                            selectedIconColor = Dark_Gray,
-                            unselectedTextColor = Gray,
-                            unselectedIconColor = Gray,
-                            indicatorColor = White
+                    items.forEach { screen ->
+                        NavigationBarItem(
+                            interactionSource = NoRippleInteractionSource,
+                            label = {
+                                Text(
+                                    text = stringResource(id = screen.resourceId),
+                                    style = B4_R
+                                )
+                            },
+                            selected = currentRoute == screen.route,
+                            colors = NavigationBarItemDefaults.colors(
+                                selectedTextColor = Dark_Gray,
+                                selectedIconColor = Dark_Gray,
+                                unselectedTextColor = Gray,
+                                unselectedIconColor = Gray,
+                                indicatorColor = White
                             ),
-                        onClick = {
-                            navController.navigate(screen.route) {
-                                popUpTo(navController.graph.findStartDestination().id) {
-                                    saveState = true
+                            onClick = {
+                                navController.navigate(screen.route) {
+                                    popUpTo(navController.graph.findStartDestination().id) {
+                                        saveState = true
+                                    }
+                                    launchSingleTop = true
+                                    restoreState = true
                                 }
-                                launchSingleTop = true
-                                restoreState = true
-                            }
-                        },
-                        icon = {
-                            Icon(
-                                painter = painterResource(id = screen.icon),
-                                contentDescription = screen.route,
-                                modifier = Modifier.size(24.dp) // 아이콘 크기 설정
+                            },
+                            icon = {
+                                Icon(
+                                    painter = painterResource(id = screen.icon),
+                                    contentDescription = screen.route,
+                                    modifier = Modifier.size(24.dp) // 아이콘 크기 설정
 
-                            )
-                        },
-                    )
+                                )
+                            },
+                        )
+                    }
                 }
-            }
+
         }
     ) { innerPadding ->
         NavHost(
@@ -118,7 +123,6 @@ fun MainScreen() {
         }
     }
 }
-
 
 
 @Preview(showBackground = true)
