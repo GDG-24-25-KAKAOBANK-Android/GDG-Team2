0e14e37ca34fcbbad3efde4117b5a1f27fed9d0b
jjwm10625 <choyeongseo950@gmail.com>
2024-11-26 11:49:02 +0900

    [FEAT] 홈화면 구현

---
+++ app/src/main/java/com/gdg/kakaobank/component/MyBank.kt
@@ -0,0 +1,91 @@
+// src/main/java/com/gdg/kakaobank/component/MyBank.kt
+package com.gdg.kakaobank.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.gdg.kakaobank.R
+import com.gdg.kakaobank.ui.theme.B4_R
+import com.gdg.kakaobank.ui.theme.B4_SB
+import com.gdg.kakaobank.ui.theme.Black
+import com.gdg.kakaobank.ui.theme.H6_B
+import com.gdg.kakaobank.ui.theme.Main_Yellow
+import com.gdg.kakaobank.ui.theme.Yellow
+
+@Composable
+fun MyBankComponent(
+    name: String,
+    amount: String,
+    backgroundColor: Color = Main_Yellow
+) {
+    Box(
+        modifier = Modifier
+            .size(340.dp, 140.dp)
+            .background(backgroundColor, shape = RoundedCornerShape(20.dp))
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_bank),
+            contentDescription = "KaKao Bank Icon",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .size(31.dp)
+                .align(Alignment.TopStart)
+                .offset(y = 10.dp) // 아래로 이동
+
+        )
+        Text(
+            text = "${name}의 통장",
+            style = B4_R,
+            color = Black,
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(start = 40.dp)
+                .offset(y = 10.dp) // 아래로 이동
+        )
+        Text(
+            text = "${amount}원",
+            style = H6_B,
+            color = Black,
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(start = 40.dp)
+                .offset(y = 26.dp) // 아래로 이동
+        )
+        Image(
+            painter = painterResource(id = R.drawable.more),
+            contentDescription = "More Icon",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .size(29.dp)
+                .align(Alignment.TopEnd)
+        )
+        Button(
+            onClick = { /* TODO: 이체 버튼 클릭 처리 */ },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .size(50.dp, 30.dp), // 버튼 크기 조정
+            colors = ButtonDefaults.buttonColors(containerColor = Yellow),
+            contentPadding = PaddingValues(0.dp) // 버튼 내부 패딩 조정
+
+        ) {
+            Text(
+                text = "이체",
+                style = B4_SB,
+                color = Black,
+                modifier = Modifier.padding(0.dp) // 텍스트 패딩 조정
+            )
+        }
+    }
+}
--- app/src/main/java/com/gdg/kakaobank/presentation/HomeScreen.kt
+++ app/src/main/java/com/gdg/kakaobank/presentation/HomeScreen.kt
@@ -1,25 +1,101 @@
 package com.gdg.kakaobank.presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.fillMaxSize
-import com.gdg.kakaobank.ui.theme.H1_B
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.gdg.kakaobank.component.BankComponent
+import com.gdg.kakaobank.component.MyBankComponent
+import com.gdg.kakaobank.ui.theme.Dark_Mint
+import com.gdg.kakaobank.ui.theme.Deep_Blue
+import com.gdg.kakaobank.ui.theme.Light_Mint
+import com.gdg.kakaobank.ui.theme.Pink
 import com.gdg.kakaobank.ui.theme.White
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.painterResource
+import com.gdg.kakaobank.R
+import com.gdg.kakaobank.ui.theme.B4_B
+import com.gdg.kakaobank.ui.theme.Black
+import com.gdg.kakaobank.ui.theme.H5_B
 
 @Composable
 fun HomeScreen() {
+    val bankList = listOf(
+        Pair("김나현", "700,000,000"),
+        Pair("조영서", "700,000,000"),
+        Pair("이가을", "100,000,000"),
+        Pair("백서연", "100,000,000"),
+        Pair("이현진", "100,000,000")
+    )
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(White),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+            .background(White)
+            .padding(top = 24.dp)
     ) {
-        Text("홈", style = H1_B)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "눈송이",
+                    style = H5_B,
+                    color = Black
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "내 계좌",
+                    style = B4_B,
+                    color = Black
+                )
+            }
+            Image(
+                painter = painterResource(id = R.drawable.bell),
+                contentDescription = "Bell Icon",
+                modifier = Modifier
+                    .size(30.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(White),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            items(1) {
+                MyBankComponent(name = "눈송이", amount = "100,000,000")
+                Spacer(modifier = Modifier.height(12.dp))
+            }
+            items(bankList.size) { index ->
+                val colors = listOf(Pink, Dark_Mint, Light_Mint, Deep_Blue)
+                val (name, amount) = bankList[index]
+                BankComponent(
+                    name = name,
+                    amount = amount,
+                    backgroundColor = colors[index % colors.size]
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+            }
+        }
+        Spacer(modifier = Modifier.height(20.dp))
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomComponentPreview() {
+    HomeScreen()
 }
